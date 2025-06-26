fd650ac9f3b7555a1fa730c005098c91ce1c9054
asmaaHamdym <asmahamdym@gmail.com>
2025-06-26 21:45:13 +0300

    Add vuex-persistedstate for state persistence in Vuex store even if the page reloaded

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,7 @@ import { createStore } from 'vuex'
 import ProductsModule from './modules/products'
 import CartModule from './modules/cart'
 import selectedProductModule from './modules/selectedProduct'
+import createPersistedState from 'vuex-persistedstate'
 
 export default createStore({
   modules: {
@@ -13,4 +14,9 @@ export default createStore({
   mutations: {},
   actions: {},
   getters: {},
+  plugins: [
+    createPersistedState({
+      paths: ['cart', 'selectedProduct'], // persist only the cart and selectedProduct modules
+    }),
+  ],
 })
